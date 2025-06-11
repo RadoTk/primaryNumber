39a372e55f3985c2fd8ad3d228a2030e4e785dcf
Papillon <hei.rado.3@gmail.com>
2025-06-11 10:22:05 +0300

    feat: add HelloWorldService

--- src/main/java/school/hei/primaryNumber/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/school/hei/primaryNumber/endpoint/rest/controller/HelloWorldController.java
@@ -1,13 +1,19 @@
 package school.hei.primaryNumber.endpoint.rest.controller;
 
+
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import school.hei.primaryNumber.service.HelloWorldService;
 
 @RestController
+@AllArgsConstructor
 public class HelloWorldController {
+    private final HelloWorldService service;
 
     @GetMapping("/hello")
-    public String helloWorld() {
-        return "... world!";
+    public String helloWorld(@RequestParam String name) {
+        return service.uploadHelloWorldMessage(name);
     }
 }
